2d2d37c51de6072a6f4cfd9fd20f6d4b195eb7ea
Matheus Aquino <teteuaquinao01@hotmail.com>
2022-08-17 14:34:51 -0300

    Projeto em JavaScript, mostrando a manupulação do DOM.

---
+++ etapas.js
@@ -0,0 +1,72 @@
+let etapas = [
+    {  
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '14200',
+        nome: 'ADILSON AMADEU',
+        partido: 'PTB',
+        fotos:[
+            {url:'img/Vereador.1.jpg', legenda: 'Vereador'}
+        ]
+      },
+
+      {
+        numero: '77222',
+        nome: 'Marlon Luz',
+        partido: 'MDB',
+        fotos:[
+            {url:'img/marlon-1.jpg', legenda: 'Vereador',}
+        ]
+      },
+      {
+        numero: '59080',
+        nome: 'Sandra Santana',
+        partido: 'PSDB',
+        fotos:[
+            {url:'img/sandra.jpg', legenda: 'Vereador',}
+        ]
+      },
+    ]
+
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+        {
+            numero: '13',
+            nome:'Luiz Inacio Lula da Silva',
+            partido: 'PT',
+            vice: 'Geraldo Alkimen',
+            fotos:[
+                {url: 'img/lula.jpeg', legenda: 'Prefeito'},
+                {url: 'img/geraldo.jpeg', legenda: 'Vice-Prefeito', small: true}
+            ]
+        },
+
+        {
+            numero: '17',
+            nome:'Jair Bolsonaro',
+            partido: 'PL',
+            vice: 'Braga',
+            fotos:[
+                {url: 'img/Bolsonaro.jpeg', legenda: 'Prefeito'},
+                {url: 'img/braga.jpeg', legenda: 'Vice-Prefeito', small: true}
+            ]
+        },
+
+        {
+            numero: '12',
+            nome:'Ciro Gomes',
+            partido: 'PDT',
+            vice: 'Ana Paula',
+            fotos:[
+                {url: 'img/gomes.jpeg', legenda: 'Prefeito'},
+                {url: 'img/ana.jpeg', legenda: 'Vice-Prefeito', small: true}
+            ]
+        },
+    ]
+    }
+];
